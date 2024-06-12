10fd1aece6eaa7833d980ef49d58aa0ef20f7b29
JinalParikh25 <theparikh303@gmail.com>
2024-06-12 12:12:25 +0200

    Project setup

---
+++ RestaurantTableBookingApp.Core/DiningTable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace RestaurantTableBookingApp.Core;
+
+public partial class DiningTable
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public int BranchId { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    [Unicode(false)]
+    public string? SeatsName { get; set; }
+
+    [Required]
+    public int Capacity { get; set; }
+
+    [ForeignKey("BranchId")]
+    [InverseProperty("DiningTables")]
+    public virtual RestaurantBranch Branch { get; set; } = null!;
+
+    [InverseProperty("Table")]
+    public virtual ICollection<Reservation> Reservations { get; set; } = new List<Reservation>();
+}
---
+++ RestaurantTableBookingApp.Core/TimeSlot.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace RestaurantTableBookingApp.Core;
+
+public partial class TimeSlot
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public int BranchId { get; set; }
+
+    [Required]
+    public DateOnly ReservationDay { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    [Unicode(false)]
+    public string MealType { get; set; } = null!;
+
+    [Required]
+    [StringLength(50)]
+    [Unicode(false)]
+    public string TableStatus { get; set; } = null!;
+
+    [ForeignKey("BranchId")]
+    [InverseProperty("TimeSlots")]
+    public virtual RestaurantBranch Branch { get; set; } = null!;
+
+    [InverseProperty("TimeSlot")]
+    public virtual ICollection<Reservation> Reservations { get; set; } = new List<Reservation>();
+}
